8718c9a6de63ef253d175dc961b82f995dbec866
Mitchell Bylak <mjbylak@uark.edu>
2022-11-29 22:53:07 -0600

    finalized_kruskal

--- src/algorithms/dijkstra.cpp
+++ src/algorithms/dijkstra.cpp
@@ -8,12 +8,25 @@ struct EdgeKeyComparison {
 };
 
 std::vector<int> searchShortestPath(Graph &G, int start, int destination) {
-    
-    // std::priority_queue< Edge, std::vector<Edge>, EdgeKeyComparison > heap;
-    // If you want to use heap to optimize the minimum searching, you can use heap defined as above
-    //      Insert: heap.push(Edge(u, -1, distance));
-    //      Get Minimum: top = heap.top(); u = top.u; distance = top.w;
-    //      Remove top: heap.pop(); (this function usually goes after the get minimum method)
-    
-    // YOUR CODE HERE
+    // distance[start] ← 0
+    // parent[start] ← -1
+    // for i from 1 to |V| do
+    // u ← argmin
+    // ! ∈ # &'( ) *+ ',- )*+*-.(
+    // distance[u]
+    // visited[u] ← True
+    // Break if v is a destination
+    // for each vertex v in the adjacency list of u do
+    // if distance[u] + w(u, v) < distance[v] then
+    // distance[v] ← distance[u] + w(u, v) and parent[v] ← u
+    // end if
+    // end do
+    // end do
+    // Path ← { }
+    // u ← destination
+    // while u ≠ -1 do
+    // Path ← { u } ∪ Path
+    // u ← parent[u]
+    // end do
+    // return Path 
 }
