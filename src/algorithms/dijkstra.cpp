3c7281b94ff6e1b91afe4dc3d1ea19c98078b848
Mitchell Bylak <mjbylak@uark.edu>
2022-11-30 21:09:57 -0600

    bugFix

--- src/algorithms/dijkstra.cpp
+++ src/algorithms/dijkstra.cpp
@@ -9,33 +9,27 @@ struct EdgeKeyComparison {
 };
 
 std::vector<int> searchShortestPath(Graph &G, int start, int destination) {
+    
     G.distance(start) = 0;
-
     std::vector<int> path = {};
-
     int v = start;
 
+    
     for(int i = 0; i < G.n; i++) {
-        int min = INT_MAX;
-
+        int min = 10000;
         for(int j = 0; j < G.n; j++) {
-
             if(G.distance(j) < min && !G.isVisited(j)) {
                 min = j;
                 v = j;
             }
-
         }
-
         G.setVisited(v);
-
         if(v == destination)
             break;
 
         for(int i = 0; i < G.e[v].size(); i++) {
             int neighbor = G.e[v][i].v;
             int cost = G.e[v][i].w + G.distance(v);
-
             if(cost < G.distance(neighbor)) {
                 G.distance(neighbor) = cost;
                 G.setTrace(neighbor, v);
